62ed39c8d525b7afff7131109560d7c1297050ba
Sajid Zaidi <sajidsrzaidi@gmail.com>
2021-05-13 18:49:51 -0700

    just drop 4 columns

--- biopan_csv.py
+++ biopan_csv.py
@@ -1,15 +1,20 @@
 import sys
-import csv 
+
+import pandas as pd
 
 if __name__ == "__main__":
     if len(sys.argv) <= 2:
         print("biopan_csv.py inputfile outputfile")
         exit(1)
-    input_file= sys.argv[0]
-    output_file=sys.argv[1]
+    input_file= sys.argv[1]
+    output_file=sys.argv[2]
 
-    with open(input_file) as f:
-        reader=csv.reader(f)
-        for row in reader:
-            print(row)
+
+    
+    df=pd.read_csv(input_file)
+    cols_to_drop = [1,2,3,4]
+    new_df=df.drop(df.columns[cols_to_drop],axis=1)
+
+    new_df.to_csv(output_file,index=False)
+                
 
